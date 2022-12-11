28f595d50396b2b6ff28d2f7799ab38399d8498e
stevenliu0724 <steven.liu0724@gmail.com>
2022-12-11 15:23:45 +0800

    Star.js

--- Star.js
+++ Star.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById("cvs");
 const c = canvas.getContext("2d");
 const stars = [];
 const ministars = [];
+const backgroundstars = [];
 
 
 canvas.width = window.innerWidth;
@@ -11,7 +12,7 @@ const backgroundGradient = c.createLinearGradient(0, 0, 0, canvas.width);
 backgroundGradient.addColorStop(0, "#171e26");
 backgroundGradient.addColorStop(0, "#3f586b");
 
-const color = "blue";
+const color = "white";
 
 class Star {
     constructor(x, y, r, color) {
@@ -28,11 +29,15 @@ class Star {
     };
 
     draw() {
+        //c.save();
         c.beginPath();
         c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
         c.fill();
         c.fillStyle = this.color;
+        c.shadowColor = "white";
+        c.shadowBlur = 0.5;
         c.closePath();
+        //c.restore();
     };
 
     update() {
@@ -66,17 +71,21 @@ class Ministar {
             y: randomXY(-15, 15),
         };
         this.friction = 0.8;
-        this.gravity = 0.1;
+        this.gravity = 0.3;
         this.ttl = 100;
         this.opacity = 1;
     };
 
     draw() {
+        c.save();
         c.beginPath();
         c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
         c.fill();
         c.fillStyle = `rgba(255, 255, 255, ${this.opacity})`;
+        c.shadowColor = "white";
+        c.shadowBlur = 20;
         c.closePath();
+        c.restore();
     };
 
     update() {
@@ -99,6 +108,13 @@ function init() {
 
     for (let i = 0; i < 8; i++) {
         stars.push(new Star(canvas.width / 2, 30, 30, "white"))  
+
+    for (let i = 0; i < 8; i++) {
+        const x = Math.random() * canvas.width
+        const y = Math.random() * canvas.height
+        const r = Math.random() * 3
+        backgroundstars.push(new Star(x, y, r, "white"))
+    }
     };
 };  
 init();
@@ -108,12 +124,22 @@ function animate() {
     c.fillStyle = backgroundGradient;
     c.fillRect(0, 0, canvas.width, canvas.height);
     
+    backgroundstars.forEach((backgroundstar) => {
+        backgroundstar.draw();
+    }); 
+
+    createMontain(1, canvas.height - 50, "#384551");
+    createMontain(2, canvas.height - 100, "#283843");
+    createMontain(3, canvas.height - 300, "#26333E");
+
     stars.forEach((star, index) => {
         star.update();
         if(star.r == 0) {
             stars.splice(index, 1);
         }
     });
+
+
     
     ministars.forEach((ministar, index) => {
         ministar.update();
@@ -121,14 +147,27 @@ function animate() {
             ministars.splice(index, 1);
         }
     });
+
 };
 
 
 animate();
 
-
-
-
+function createMontain(mountainAmount, height, color) {
+    for (let i = 0; i < mountainAmount; i++) {
+        const mountainWidth = canvas.width / 3;
+        c.save();
+        c.beginPath();
+        c.moveTo(i * mountainWidth, canvas.height);
+        c.lineTo(i * mountainWidth + mountainWidth + 325, canvas.height);
+        c.lineTo(i * mountainWidth + mountainWidth / 2, canvas.height - height);
+        c.lineTo(i * mountainWidth - 325, c.canvas.height);
+        c.fillStyle = color;
+        c.fill();
+        c.closePath();
+        c.restore();   
+    }
+}
 
 function randomXY(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
